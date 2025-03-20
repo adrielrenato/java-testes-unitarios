c6177561b3ed26c7ee449d947adcbcede612f5b8
Adriel Renato <adriel.renato26@gmail.com>
2025-03-20 20:13:19 -0300

    Criando cenario para testes automatizados de validacao se o pet está com adoção em andamento ou não

---
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamentoTest.java
@@ -0,0 +1,49 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ValidacaoPetComAdocaoEmAndamentoTest {
+
+    @InjectMocks
+    private ValidacaoPetComAdocaoEmAndamento validacao;
+
+    @Mock
+    private AdocaoRepository adocaoRepository;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+    @Test
+    void deveriaPermitirSolicitacaoDePetPetNaoTemAdocaoEmAndamento() {
+        // ARRANGE
+        BDDMockito.given(adocaoRepository.existsByPetIdAndStatus(
+                dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO
+        )).willReturn(false);
+
+        // ACT + ASSERT
+        Assertions.assertDoesNotThrow(() -> validacao.validar(dto));
+    }
+
+    @Test
+    void naoDeveriaPermitirSolicitacaoPetComAdocaoEmAndamento() {
+        // ARRANGE
+        BDDMockito.given(adocaoRepository.existsByPetIdAndStatus(
+                dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO
+        )).willReturn(true);
+
+        // ACT + ASSERT
+        Assertions.assertThrows(ValidacaoException.class, () -> validacao.validar(dto));
+    }
+
+}
