1db3c8b60577153ab661be927dc6dbbd504e554e
Adriel Renato <adriel.renato26@gmail.com>
2025-03-20 18:28:39 -0300

    Criando um cenario para verificar se retorna uma probilidade alta de adoção de um pet

---
+++ src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -0,0 +1,37 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.dto.CadastroPetDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
+import br.com.alura.adopet.api.model.TipoPet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraProbabilidadeAdocaoTest {
+    @Test
+    @DisplayName("Idade 4 anos e 4kg - ALTA")
+    void cenario01() {
+        var abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo Feliz",
+                "11999999999",
+                "abrigofeliz@email.com.br"
+        ));
+        var pet = new Pet(new CadastroPetDto(
+                TipoPet.GATO,
+                "Miau",
+                "Siames",
+                4,
+                "Cinza",
+                4.0f
+        ), abrigo);
+        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+        ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
+
+        Assertions.assertEquals(ProbabilidadeAdocao.ALTA, probabilidade);
+    }
+}
